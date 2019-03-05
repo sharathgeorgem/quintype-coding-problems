4e0e5c0a348962b3c53d82253f2e86882d186484
Sharath George M <sharathgeorgem@gmail.com>
2019-03-05 16:17:23 +0530

    Added functionality to find position of corresponding div for every click

--- diamond-sweeper/app/client/app.js
+++ diamond-sweeper/app/client/app.js
@@ -3,7 +3,7 @@ global.startApp = function (container) {
   let cells = document.getElementsByClassName('cell')
 
   for (let cell of cells) {
-    cell.addEventListener('click', sayHi)
+    cell.addEventListener('click', checkForDiamond)
     if (!(cell.classList.contains('unknown'))) {
       cell.classList += ' unknown'
     }
@@ -12,7 +12,17 @@ global.startApp = function (container) {
     }
   }
 
-  function sayHi () {
+  function checkForDiamond (e) {
+    console.log('When you click you get all this', e)
+    let elementToCheck = e.srcElement
+    let position = elementToCheck.getBoundingClientRect()
+    console.log('The top and the left positions are ', position.top, position.left)
+    elementToCheck.classList.remove('unknown')
+    if (elementToCheck.classList.contains('diamond')) {
+      console.log('You just found a diamond')
+    } else {
+      elementToCheck.classList.add('arrow')
+    }
     console.log(`The number of diamonds are ${countOfDiamonds} `)
   }
 }
