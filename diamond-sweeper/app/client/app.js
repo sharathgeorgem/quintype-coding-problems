de0660d55f98587c128896e60a49d4972fcafa06
Sharath George M <sharathgeorgem@gmail.com>
2019-03-06 15:31:09 +0530

    Fixed arrows pointing to wrong direction issue

--- diamond-sweeper/app/client/app.js
+++ diamond-sweeper/app/client/app.js
@@ -1,6 +1,16 @@
 global.startApp = function (container) {
   let countOfDiamonds = 0
   let arrayOfDiamonds = []
+  let arrowDirections = {
+    'north': 'rotate(-90deg)',
+    'south': 'rotate(-270deg)',
+    'east': 'rotate(-180deg)',
+    'west': 'rotate(0deg)',
+    'north-east': 'rotate(-45deg)',
+    'north-west': 'rotate(-135deg)',
+    'south-east': 'rotate(45deg)',
+    'south-west': 'rotate(135deg)'
+  }
   let cells = document.getElementsByClassName('cell')
 
   for (let cell of cells) {
@@ -39,12 +49,14 @@ global.startApp = function (container) {
       let diamondPosition = arrayOfDiamonds[closestDiamondIndex]
       console.log('The diamond position to be passed is ', diamondPosition)
       let typeOfArrow = findTypeOfArrow(elementPosition, diamondPosition)
+
       console.log('The type of arrow is ', typeOfArrow)
       console.log('The diamond position : ', diamondPosition)
       console.log('The elementClickedOnPosition is ', elementPositionObj)
 
       console.log('The new array for the type of arrows is given as ', dynamicDistanceArray)
       elementToCheck.classList.add('arrow')
+      elementToCheck.style['transform'] = arrowDirections[typeOfArrow]
       setTimeout(() => elementToCheck.classList.remove('arrow'), 1000)
     }
 
@@ -58,13 +70,16 @@ global.startApp = function (container) {
     let y0 = parseInt(originObj.top)
     let y1 = targetObj.cellPositionTop
     console.log(x0, x1, y0, y1)
+    // if (Math.abs(x1 - x0) > Math.abs(y1 - y0)) {
+    // Debug
+    // }
     if (x0 === x1) {
       if (y0 > y1) return 'north'
       return 'south'
     }
     if (y0 === y1) {
-      if (x0 > x1) return 'west'
-      return 'east'
+      if (x0 > x1) return 'east'
+      return 'west'
     }
     if (x0 < x1 && y0 < y1) return 'south-east'
     if (x0 > x1 && y0 < y1) return 'south-west'
