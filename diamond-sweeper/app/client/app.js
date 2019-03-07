bb59624298079165cd7105fbbfedfb95c90c3476
Sharath George M <sharathgeorgem@gmail.com>
2019-03-07 12:24:56 +0530

    Added comments

--- diamond-sweeper/app/client/app.js
+++ diamond-sweeper/app/client/app.js
@@ -1,8 +1,12 @@
 global.startApp = function (container) {
+  /* Initialization */
+
   let countOfDiamonds = 0
   let diamondsUncovered = 0
   let arrayOfDiamonds = []
   let initialScore = 64
+
+  /* As the given arrow svg points to west, the other directions are adjusted accordingly */
   let arrowDirections = {
     'north': 'rotate(-90deg)',
     'south': 'rotate(-270deg)',
@@ -13,16 +17,21 @@ global.startApp = function (container) {
     'south-east': 'rotate(45deg)',
     'south-west': 'rotate(135deg)'
   }
-  let cells = document.getElementsByClassName('cell')
+
   let modal = document.querySelector('.modal')
+
   let restartGame = document.querySelector('.restart-game')
   restartGame.addEventListener('click', initialize)
 
+  /* Add unknown class to all the table cells before the game starts */
+  let cells = document.getElementsByClassName('cell')
   for (let cell of cells) {
     cell.addEventListener('click', checkForDiamond)
+
     if (!(cell.classList.contains('unknown'))) {
       cell.classList += ' unknown'
     }
+    /* If cell contains a diamond, add its position to the distance array for distance calculation */
     if (cell.classList.contains('diamond')) {
       ++countOfDiamonds
       let cellBounds = cell.getBoundingClientRect()
@@ -33,6 +42,7 @@ global.startApp = function (container) {
     }
   }
 
+  /* To display initial score */
   let score = document.getElementById('score')
   score.textContent = 'Score : ' + initialScore
 
@@ -40,6 +50,7 @@ global.startApp = function (container) {
     window.location.reload()
   }
 
+  /* To check for presence of diamond on click */
   function checkForDiamond (e) {
     let elementToCheck = e.srcElement
     let elementPosition = elementToCheck.getBoundingClientRect()
@@ -52,6 +63,8 @@ global.startApp = function (container) {
 
     if (elementToCheck.classList.contains('diamond')) {
       elementToCheck.parentNode.style['background-color'] = '#78e08f'
+
+      /* When all diamonds are uncovered display score using modal */
       if (diamondsUncovered === 7) {
         let score = document.getElementById('final-score')
         score.textContent = 'Your score : ' + initialScore
@@ -59,8 +72,10 @@ global.startApp = function (container) {
       }
       ++diamondsUncovered
       console.log('You just found a diamond')
-      // arrayOfDiamonds.splice(arrayOfDiamonds.indexOf(elementPositionObj), 1)
+      // arrayOfDiamonds.splice(arrayOfDiamonds.indexOf(elementPositionObj), 1) // debug
     } else {
+
+      /* To display arrow according to location of the nearest diamond */
       elementToCheck.parentNode.style['background-color'] = '#2e383a'
       let dynamicDistanceArray = findTheNearestDiamond(elementPositionObj)
       let closestDiamondIndex = dynamicDistanceArray.indexOf(Math.min(...dynamicDistanceArray))
@@ -97,6 +112,7 @@ global.startApp = function (container) {
     if (x0 < x1 && y0 > y1) return 'north-east'
   }
 
+  /* Returns a distance array which consists of the distances between clicked square and other diamonds */
   function findTheNearestDiamond (diamondPositionObj) {
     let distanceArr = arrayOfDiamonds.map((item, index, arr) => {
       return parseInt(Math.sqrt(Math.pow((item.cellPositionLeft - diamondPositionObj.cellPositionLeft), 2) +
